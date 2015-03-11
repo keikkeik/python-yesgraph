e706f4f8129ac8840aa0a0335e1c745d08e2f8bd
George Hickman <george@ghickman.co.uk>
2015-03-11 09:04:20 +0000

    Add trove classifiers

--- setup.py
+++ setup.py
@@ -19,4 +19,13 @@ setup(
     zip_safe=False,
     platforms='any',
     install_requires=dependencies,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
 )
