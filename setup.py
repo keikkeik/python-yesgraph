ee77ff5c8358b6d398440572eb56663def1327dd
Vincent Driessen <me@nvie.com>
2015-03-24 10:10:18 +0100

    This is 0.2.0.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ dependencies = ['requests', 'six']
 
 setup(
     name='yesgraph',
-    version='0.1.3',
+    version='0.2.0',
     url='https://github.com/yesgraph/python-yesgraph',
     author='YesGraph',
     author_email='team@yesgraph.com',
