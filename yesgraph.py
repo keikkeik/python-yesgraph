d8a0968046b7a7a9bcff8a3b98e1cd02fcaf07d9
Jonathan Chu <jonathan.chu@me.com>
2015-03-26 23:37:11 -0400

    Add GET /address-books admin endpoint wrapper.

--- yesgraph.py
+++ yesgraph.py
@@ -161,3 +161,11 @@ class YesGraphAPI(object):
         Documentation - https://www.yesgraph.com/docs/#post-users
         """
         return self._request('POST', '/users', users)
+
+    def get_address_books(self):
+        """
+        Wrapped method for GET of /address-books endpoint
+
+        Documentation - https://www.yesgraph.com/docs/reference#get-address-books
+        """
+        return self._request('GET', '/address-books')
